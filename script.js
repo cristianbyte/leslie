8b44dac41e8e9f36504fb26f5048e44f7f0bdc70
charls <charlsmaritz@gmail.com>
2023-09-18 15:18:54 -0500

    update(card.css) animations with javascript and css

--- script.js
+++ script.js
@@ -10,8 +10,33 @@ play.addEventListener('click', ()=>{
   },3000)
 })
 
+// CARDS ##########
 
+const card1 = document.getElementById('card1')
+const card2 = document.getElementById('card2')
+const card3 = document.getElementById('card3')
 
+card1.addEventListener('click', ()=>{
+  card1.style.zIndex = '100'
+  card1.style.animation = 'zoomflip 1s ease-in'
+  setTimeout(()=>{
+    card1.style.transform = 'scale(2) translateX(calc(50% + 2rem))'
+  },1000)
+})
+card2.addEventListener('click', ()=>{
+  card2.style.zIndex = '100'
+  card2.style.animation = 'zoomflip2 1s ease-in'
+  setTimeout(()=>{
+    card2.style.transform = 'scale(2)'
+  },1000)
+})
+card3.addEventListener('click', ()=>{
+  card3.style.zIndex = '100'
+  card3.style.animation = 'zoomflip3 1s ease-in'
+  setTimeout(()=>{
+    card3.style.transform = 'scale(2) translateX(calc(-50% - 2rem))'
+  },1000)
+})
 
 
 // Obtén todos los elementos <p> con el atributo data-content
