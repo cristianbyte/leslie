af4a90c7b74e28a92ce0891a04623f6b354900c9
cristianbyte <charlsmaritz@gmail.com>
2024-03-19 15:51:21 -0500

    ramdon icons, letter

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,37 @@
 let body = document.getElementsByTagName('body')[0];
+let numberGrids = Math.floor(window.innerWidth/300 + window.innerHeight/200);
+const gridWidth = Math.floor( window.innerWidth / 50);
+const gridHeight = Math.floor( window.innerHeight / 50);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+(function (){
+  for(let i = 0;i < numberGrids ;i++){
+    let fill = document.createElement("div");
+    let heart = document.createElement("div")
+    let randomX = getRandomInt(gridWidth);
+    let randomY = getRandomInt(gridHeight);
+    console.log(randomX);
+    fill.classList.add('fill');
+    fill.style.top = ((randomY * 50) - 2 ) + "px";
+    fill.style.left = ((randomX * 50) - 2 ) + "px";
+    heart.classList.add('heart');
+    heart.style.top = getRandomInt(gridHeight*50) + "px";
+    heart.style.left = getRandomInt(gridWidth*50) + "px";
+    console.log(getRandomInt(gridHeight));
+    
+    body.appendChild(fill);
+    body.appendChild(heart);
+  } 
+})();
 
-for(let i = 0;i < 8;i++){
-  let fill = document.createElement("div");
-  let randomX = getRandomInt(41);
-  let randomY = getRandomInt(21);
-  fill.classList.add('fill');
-  fill.style.top = ((randomY * 50) - 2) + "px";
-  fill.style.left = ((randomX * 50) - 2) + "px";
-  console.log(randomX ,randomY);
+window.addEventListener("DOMContentLoaded",()=>{
+  folderStart();
+});
 
-  body.appendChild(fill);
-} 
+function folderStart(){
+  const folder = document.querySelector(".folder")
+  folder.classList.add('animatedFolder')
+}
