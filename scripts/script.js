dd17255441944af0ea37403839884bdbc49be727
cristianbyte <charlsmaritz@gmail.com>
2024-03-20 15:48:55 -0500

    request images progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let body = document.getElementsByTagName('body')[0];
+const folder = document.querySelector(".folder")
 let numberGrids = Math.floor(window.innerWidth/300 + window.innerHeight/200);
 const gridWidth = Math.floor( window.innerWidth / 50);
 const gridHeight = Math.floor( window.innerHeight / 50);
@@ -27,11 +28,45 @@ function getRandomInt(max) {
   } 
 })();
 
-window.addEventListener("DOMContentLoaded",()=>{
-  folderStart();
+window.addEventListener("DOMContentLoaded", async ()=>{
+  await folderStart();
+  loadIputTypeFiles();
+  relaseContent();
 });
 
-function folderStart(){
-  const folder = document.querySelector(".folder")
-  folder.classList.add('animatedFolder')
+function loadIputTypeFiles(){
+  let input = document.createElement('input');
+  input.type = "file";
+  input.accept = "images/*";
+  input.multiple = true;
+  input.style.display = "none";
+  body.appendChild(input);
+  input.click();
+}
+
+function relaseContent(){
+  let pastPos = undefined;
+  let actualPos = undefined;
+  const traking = setInterval(()=>{
+    actualPos = folder.getBoundingClientRect().left;
+    console.log(actualPos);
+    if(pastPos == actualPos){
+      clearInterval(traking)
+    }else{
+      pastPos = folder.getBoundingClientRect().left;
+    }
+  },50)
+}
+
+async function folderStart(){
+  return new Promise((resolve) =>{
+    folder.addEventListener("click", ()=>{
+      const open = document.querySelector(".open");
+      open.classList.add('open--folder');
+      setTimeout(()=>{
+        folder.classList.add('folder__moved');
+        resolve();
+      },400)
+    });
+  });
 }
